3ad5c3de43b0a82254e55c83876f982c919d3288
Julian Kaufmann <16280297+kfmndev@users.noreply.github.com>
2026-09-17 02:15:51 +0200

    feat(logging): pad level text and pin text output to stderr

--- internal/config/config.go
+++ internal/config/config.go
@@ -68,7 +68,9 @@ func SetupLogging() {
 			TimestampFormat: "2006-01-02 15:04:05",
 			FullTimestamp:   true,
 			ForceColors:     true,
+			PadLevelText:    true,
 		})
+		log.SetOutput(os.Stderr)
 	}
 	log.SetLevel(GetLogLevel())
 }
